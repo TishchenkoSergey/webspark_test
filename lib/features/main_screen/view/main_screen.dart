0bb65c9e7ab9e19cb337c7557867402985bf8868
Serhii <tishchenko.seriy@gmail.com>
2025-04-24 00:08:44 +0300

    refactor: move link input validation to bloc

--- lib/features/main_screen/view/main_screen.dart
+++ lib/features/main_screen/view/main_screen.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
-import '../../../widgets/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:webspark_test/features/main_screen/bloc/main_cubit.dart';
+import 'package:webspark_test/widgets/widgets.dart';
+
+import '../extensions/extensions.dart';
 
 class MainScreen extends StatelessWidget {
   MainScreen({super.key});
@@ -16,59 +21,60 @@ class MainScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Screen'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Set valid API base URL in order to continue',
-                style: Theme.of(context).textTheme.titleMedium,
-              ),
-              Row(
+    return BlocBuilder<MainCubit, MainState>(
+      builder: (BuildContext context, MainState state) {
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text('Home Screen'),
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  const Icon(Icons.compare_arrows_outlined),
-                  const SizedBox(width: 24),
-                  Expanded(
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                        border: const UnderlineInputBorder(),
-                        labelStyle: textTheme.bodyLarge,
-                        hintStyle: textTheme.labelMedium?.copyWith(color: Colors.black.withValues(alpha: 0.65)),
-                      ),
-                      keyboardType: TextInputType.url,
-                      validator: (value) {
-                        if (value == null || value.trim().isEmpty) {
-                          return 'Field cannot be empty.';
-                        } else if (!isValidUrl(value.trim())) {
-                          return 'Invalid URL.';
-                        }
-                        return null;
-                      },
-                    ),
+                  Text(
+                    'Set valid API base URL in order to continue',
+                    style: Theme.of(context).textTheme.titleMedium,
                   ),
+                  Row(
+                    children: [
+                      const Icon(Icons.compare_arrows_outlined),
+                      const SizedBox(width: 24),
+                      Expanded(
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            border: const UnderlineInputBorder(),
+                            errorText: state.linkInput.errorMessage(context),
+                            labelStyle: textTheme.bodyLarge,
+                            hintStyle: textTheme.labelMedium?.copyWith(color: Colors.black.withValues(alpha: 0.65)),
+                          ),
+                          onChanged: context.read<MainCubit>().linkChanged,
+                          keyboardType: TextInputType.url,
+                        ),
+                      ),
+                    ],
+                  )
                 ],
-              )
-            ],
+              ),
+            ),
           ),
-        ),
-      ),
-      bottomSheet: Padding(
-        padding: const EdgeInsets.all(16),
-        child: SizedBox(
-          width: double.infinity,
-          child: CustomButton(
-            title: 'Start counting process',
-            onPressed: () {},
+          bottomSheet: Padding(
+            padding: const EdgeInsets.all(16),
+            child: SizedBox(
+              width: double.infinity,
+              child: CustomButton(
+                title: 'Start counting process',
+                onPressed: () {
+                  context.read<MainCubit>().linkChanged(state.linkInput.value);
+                  context.read<MainCubit>().setNewLink();
+                },
+              ),
+            ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
