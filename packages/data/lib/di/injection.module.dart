999e81dce163e33aee130dfadaf86627b81c4fc1
Serhii <tishchenko.seriy@gmail.com>
2025-04-23 22:16:04 +0300

    feat(data, domain): update di module

--- packages/data/lib/di/injection.module.dart
+++ packages/data/lib/di/injection.module.dart
@@ -6,10 +6,16 @@
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'dart:async' as _i687;
 
+import 'package:data/repository/url_storage_repository.dart' as _i899;
+import 'package:domain/domain.dart' as _i494;
 import 'package:injectable/injectable.dart' as _i526;
+import 'package:shared_preferences/shared_preferences.dart' as _i460;
 
 class DataPackageModule extends _i526.MicroPackageModule {
 // initializes the registration of main-scope dependencies inside of GetIt
   @override
-  _i687.FutureOr<void> init(_i526.GetItHelper gh) {}
+  _i687.FutureOr<void> init(_i526.GetItHelper gh) {
+    gh.factory<_i494.UrlStorageRepository>(
+        () => _i899.UrlStorageRepositoryImpl(gh<_i460.SharedPreferences>()));
+  }
 }
