77f598b9116832c6c888890cc336c69b11ca9a16
Serhii <tishchenko.seriy@gmail.com>
2025-04-23 13:50:57 +0300

    Init project

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const WebsparkTestApp());
+}
+
+class WebsparkTestApp extends StatelessWidget {
+  const WebsparkTestApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
