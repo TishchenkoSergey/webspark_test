dfee40bc7c056fd8c91933ae4b2c0a5b28f4d320
Serhii <tishchenko.seriy@gmail.com>
2025-04-23 20:23:35 +0300

    feat(data, domain): update di dependencies

--- packages/data/lib/di/injection.dart
+++ packages/data/lib/di/injection.dart
@@ -1,10 +1,15 @@
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
 
+import 'package:shared_preferences/shared_preferences.dart';
+
 late GetIt di;
 
 @InjectableInit.microPackage()
 void initMicroPackage() {}
 
 @module
-abstract class RegisterModule {}
+abstract class RegisterModule {
+  @preResolve
+  Future<SharedPreferences> prefs() => SharedPreferences.getInstance();
+}
--- packages/data/lib/di/injection.module.dart
+++ packages/data/lib/di/injection.module.dart
@@ -6,6 +6,7 @@
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'dart:async' as _i687;
 
+import 'package:data/di/injection.dart' as _i237;
 import 'package:data/repository/url_storage_repository.dart' as _i899;
 import 'package:domain/domain.dart' as _i494;
 import 'package:injectable/injectable.dart' as _i526;
@@ -14,8 +15,15 @@ import 'package:shared_preferences/shared_preferences.dart' as _i460;
 class DataPackageModule extends _i526.MicroPackageModule {
 // initializes the registration of main-scope dependencies inside of GetIt
   @override
-  _i687.FutureOr<void> init(_i526.GetItHelper gh) {
+  _i687.FutureOr<void> init(_i526.GetItHelper gh) async {
+    final registerModule = _$RegisterModule();
+    await gh.factoryAsync<_i460.SharedPreferences>(
+      () => registerModule.prefs(),
+      preResolve: true,
+    );
     gh.factory<_i494.UrlStorageRepository>(
         () => _i899.UrlStorageRepositoryImpl(gh<_i460.SharedPreferences>()));
   }
 }
+
+class _$RegisterModule extends _i237.RegisterModule {}
