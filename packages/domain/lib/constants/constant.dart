674466ca7267d4cb67ae4c4ddc33985ef072bc5d
Serhii <tishchenko.seriy@gmail.com>
2025-04-24 18:51:42 +0300

    feat(domain): add constant directions list value

---
+++ packages/domain/lib/constants/constant.dart
@@ -0,0 +1,11 @@
+// Directions from the starting point to the nearest vertices
+const kDirections = [
+  [0, 1],
+  [0, -1],
+  [1, 1],
+  [1, 0],
+  [1, -1],
+  [-1, 1],
+  [-1, 0],
+  [-1, -1],
+];
